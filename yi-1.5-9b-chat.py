a0021fab7a9d534d7b49acdcb55d71680db0d497
Yixiang Gao <yixiangg310573@gmail.com>
2024-06-12 16:01:08 +0000

    add some sample questions and a chat format script

---
+++ yi-1.5-9b-chat.py
@@ -0,0 +1,20 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+model_name = "01-ai/Yi-1.5-9B-Chat"
+
+tokenizer = AutoTokenizer.from_pretrained("01-ai/Yi-1.5-9B-Chat", use_fast=False)
+
+# Since transformers 4.35.0, the GPT-Q/AWQ model can be loaded using AutoModelForCausalLM.
+model = AutoModelForCausalLM.from_pretrained("01-ai/Yi-1.5-9B-Chat", device_map="auto", torch_dtype='auto').eval()
+
+# Prompt content: "hi"
+messages = [
+    {"role": "system", "content": "你要回答数学题，题目中涉及到数学表达式会以latex的方式来体现，你在回答时如果要用到数学表达式也用latex代码回复。"},
+    {"role": "user", "content": "已知 k \in R， \vec{a}=(2，5)， \vec{b}=(6，k), \vec{a} // \vec{b}，则k的值为？"},
+]
+
+input_ids = tokenizer.apply_chat_template(conversation=messages, tokenize=True, return_tensors='pt')
+output_ids = model.generate(input_ids.to('cuda'), eos_token_id=tokenizer.eos_token_id, max_length=4096)
+response = tokenizer.decode(output_ids[0][input_ids.shape[1]:], skip_special_tokens=True)
+
+print(response)
