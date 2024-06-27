bf75577a55abb79dcbb07bd60c3bced6b16004d9
Yixiang Gao <yixiangg310573@gmail.com>
2024-06-27 05:28:02 +0000

    need to find a more informative way to plot this

--- inspect_activations/plot_activations.py
+++ inspect_activations/plot_activations.py
@@ -2,34 +2,56 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pickle
 
-# Load the saved data
-with open("token_activations_english.pkl", "rb") as f:
+"""
+english
+124 {
+125 1
+126 ,
+127 3
+128 ,
+129 5
+130 \
+131 }$
+
+chinese
+95  {
+96  1
+97  ,
+98  3
+99  ,
+100 5
+101 \
+102 }$
+"""
+
+with open("token_activations_chinese.pkl", "rb") as f:
     token_activation_pairs = pickle.load(f)
 
+print(len(token_activation_pairs))
 # Choose multiple token-activation pairs to visualize
-num_tokens = 4  # Change this to the number of tokens you want to compare
-fig, axes = plt.subplots(num_tokens, 1, figsize=(12, 6*num_tokens), sharex=True)
+num_tokens = 50  # Change this to the number of tokens you want to compare
+# fig, axes = plt.subplots(num_tokens, 1, figsize=(12, 6*num_tokens), sharex=True)
 
-for i in range(num_tokens):
+for i in range(len(token_activation_pairs)):
     token, activations = token_activation_pairs[i]
-    print(token)
-    activation_array = np.array(activations)
+    print(i, token)
+#     activation_array = np.array(activations)
     
-    im = axes[i].imshow(activation_array, aspect='auto', cmap='viridis')
-    axes[i].set_title(f'Token: "{token}"')
-    axes[i].set_ylabel('Layer')
-    axes[i].set_yticks(range(len(activations)))
-    axes[i].set_yticklabels(range(1, len(activations) + 1))
+#     im = axes[i].imshow(activation_array, aspect='auto', cmap='viridis')
+#     axes[i].set_title(f'Token: "{token}"')
+#     axes[i].set_ylabel('Layer')
+#     axes[i].set_yticks(range(len(activations)))
+#     axes[i].set_yticklabels(range(1, len(activations) + 1))
 
-    # Add colorbar to each subplot
-    plt.colorbar(im, ax=axes[i], label='Activation Value')
+#     # Add colorbar to each subplot
+#     plt.colorbar(im, ax=axes[i], label='Activation Value')
 
-plt.xlabel('Hidden State Dimension')
-plt.suptitle('Activation Heatmaps for Multiple Tokens', fontsize=16)
-plt.tight_layout()
+# plt.xlabel('Hidden State Dimension')
+# plt.suptitle('Activation Heatmaps for Multiple Tokens', fontsize=16)
+# plt.tight_layout()
 
-# Save the figure
-plt.savefig('activation_heatmaps_multiple_tokens.png', dpi=300, bbox_inches='tight')
-plt.close()  # Close the figure to free up memory
+# # Save the figure
+# plt.savefig('activation_heatmaps_multiple_tokens.png', dpi=300, bbox_inches='tight')
+# plt.close()  # Close the figure to free up memory
 
-print("Figure saved as activation_heatmaps_multiple_tokens.png")
+# print("Figure saved as activation_heatmaps_multiple_tokens.png")
